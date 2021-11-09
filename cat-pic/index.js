d5ca530d5e843eb24c3701e699ca81d8f331cf92
Shannon Peck <shannon.peck@cornerstonenw.com>
2021-11-09 14:05:34 -0800

    'cat annotation' exercise done

--- cat-pic/index.js
+++ cat-pic/index.js
@@ -1,1 +1,9 @@
-require('cat-picture');
+'use strict'
+
+const pic = require('cat-picture');
+const image = require('lightning-image-poly');
+
+const src= pic.src;
+pic.remove();
+
+const viz = new image('#visualization', null, [src], {hullAlgorithm: 'convex'});
